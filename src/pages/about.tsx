fc8a5275679517559b5cd934f5ed1b92533a9324
Cosmeeeen <iliemcosmin@yahoo.ro>
2024-01-08 13:01:28 +0200

    add contact email address to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,6 +10,7 @@ const AboutPage = () => {
       <p>Powered by <a href='https://openai.com/' className='underline' target='_blank'>OpenAI</a>&apos;s GPT-3.5-turbo model</p>
       <p>Source code available on <a href='https://github.com/Cosmeeeen/gpt-typing-game' className='underline' target='_blank'>GitHub</a></p>
       <p>Currently in early development, expect changes in the future.</p>
+      <p>If you have any questions or suggestions related to this project, feel free to contact me at <a href='mailto: gpt-typing-game@cosmin.zip' target="_blank" className="underline">gpt-typing-game@cosmin.zip</a></p>
       <p>If you want to support the project, you can do so by <a href='https://www.buymeacoffee.com/cosmeen' className='underline' target='_blank'>buying me a coffee</a>.</p>
     </main>
   );
