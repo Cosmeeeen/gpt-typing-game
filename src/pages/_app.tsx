1b5cbef9dfbd99dd450adf4383ca742c170e71d7
Cosmeeeen <iliemcosmin@yahoo.ro>
2024-01-20 13:25:05 +0200

    setup vercel speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { api } from "~/utils/api";
 
@@ -20,6 +21,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
       </Head>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </SessionProvider>
   );
 };
