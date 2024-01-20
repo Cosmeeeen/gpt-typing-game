d0694580b0c7b34428614ae175bbf356652e8b3e
Cosmeeeen <iliemcosmin@yahoo.ro>
2024-01-20 03:58:26 +0200

    setup vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 import { api } from "~/utils/api";
 
@@ -18,6 +19,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 };
